33d59267f8fb5769859ab25b6320afd586ce5f9b
Swati Sinha <s4520sinha@gmail.com>
2021-08-24 18:10:24 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import Birthday from './BIrthday/Birthday';
+//import Dashboard from './Accordion/Dashboard';
+import MenuPge from './Menu/MenuPge';
+//import Tab from './Tabs/Tab';
+//import Review from './Reviews/Review';
+import Travel from './Tours/Travel';
+//import Main from './Forms/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    {/* <Main/> */}
+    {/* <Birthday/> */}
+     {/* <Travel/>  */}
+    {/* <Review/> */}
+    {/* <Dashboard/> */}
+    {/* <Tab/> */}
+    <MenuPge/>
+        </div>
   );
 }
 
